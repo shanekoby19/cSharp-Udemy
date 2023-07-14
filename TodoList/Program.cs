3ef79f9827392d8a3d6a5d903d3060cdde8c415d
Shane Kobylecky <skobylecky1@gmail.com>
2023-07-13 17:02:28 -0700

    Add additional notes and functionality to our TODO list app.

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,9 +1,74 @@
-﻿Console.WriteLine("Hello!");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
 
+//var userChoice = Console.ReadLine();
+
+//// todo: Handle user input.)
+//if (userChoice == "A")
+//{
+//    PrintSelectedOption("Add a TODO.");
+//}
+//else if (userChoice == "S")
+//{
+//    PrintSelectedOption("See all TODOs.");
+//}
+//else if (userChoice == "R")
+//{
+//    PrintSelectedOption("Remove a TODO.");
+//}
+//else if (userChoice == "E")
+//{
+//    PrintSelectedOption("Exit.");
+//}
+
+//void PrintSelectedOption(string selectedOption) {
+//    Console.WriteLine("User selected: " + selectedOption);
+//}
+
+//Console.WriteLine("User Input: " + userChoice);
+
+
+//// Add method
+//int Add(int num1, int num2)
+//{
+//    return num1 + num2;
+//}
+
+//int result = Add(23, 23);
+
+//Console.WriteLine("The result is: " + result);
+
+//// Is longer than 10 characters method
+
+//bool result2 = IsLongerThan10Characters("My longest message.");
+
+//bool IsLongerThan10Characters(string message)
+//{
+//    return message.Length > 10;
+//}
+
+//Console.WriteLine("Is your message longer than 10 characters? " + result2);
+
+//Console.ReadKey();
+
+
+//Console.WriteLine("Provide a number.");
+
+//int number = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("The number you entered is: " + number);
+
+//Console.ReadKey();
+
+/*  
+ *  This 
+ *  is 
+ *  a 
+ *  multi-line
+ *  comment
+*/
 
 // Variable Declaration and Initialization
 //string userInput = "A";
@@ -38,21 +103,21 @@ Console.WriteLine("[E]xit");
 
 
 ////////////////////////////// OPERATORS ///////////////////////////////
-int a = 5;
-a++;
-int b = 20;
-b--;
+//int a = 5;
+//a++;
+//int b = 20;
+//b--;
 
-Console.WriteLine("ADDITION: " + (a + b));
-Console.WriteLine("SUBTRACTION: " + (a - b));
-Console.WriteLine("MULTIPLICATION: " + a * b);
-Console.WriteLine("DIVISION: " + a / b);
+//Console.WriteLine("ADDITION: " + (a + b));
+//Console.WriteLine("SUBTRACTION: " + (a - b));
+//Console.WriteLine("MULTIPLICATION: " + a * b);
+//Console.WriteLine("DIVISION: " + a / b);
 
-Console.WriteLine("Shane" + " " + "Kobylecky," + " I am " + 23 + " years old.");
+//Console.WriteLine("Shane" + " " + "Kobylecky," + " I am " + 23 + " years old.");
 
-// Implicity Typed Variables.
-// Must be initialized. The complier has to be able to infer the type.
-var fName = "Shane";
-var age = 26;
+//// Implicity Typed Variables.
+//// Must be initialized. The complier has to be able to infer the type.
+//var fName = "Shane";
+//var age = 26;
 
-fName = "Kobylecky";
+//fName = "Kobylecky";
